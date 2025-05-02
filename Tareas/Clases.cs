6c4e2f23fca0f2f733b8fdbac2023dd3e2d38bb1
NicoMore02 <nicomore053@gmail.com>
2025-05-02 16:14:48 -0300

    primer commit

---
+++ Tareas/Clases.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+
+namespace clases
+{
+    public class Tarea 
+    {
+        [JsonPropertyName("userId")]
+        public int userId { get; set; }
+
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("tittle")]
+        public string tittle { get; set; }
+        
+        [JsonPropertyName("completed")]
+        public bool completed { get; set; }
+    }
+}
---
+++ Tareas/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using clases;
+using System.Dynamic;
+class program {
+    private static readonly HttpClient client = new HttpClient();
+    //private const string url = "https://jsonplaceholder.typicode.com/todos/";
+
+    static async Task Main(string[] args) 
+    {
+        var tareas = await GetTareas();
+        mostrarTareas(tareas);
+        
+    }
+
+    static async Task<List<Tarea>> GetTareas() 
+    {
+        HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        List<Tarea> listtareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+        return listtareas;
+    }
+
+    static void mostrarTareas(List<Tarea> tareas) 
+    {
+        Console.WriteLine("====Tareas====");
+        foreach (var tarea in tareas)
+        {
+            Console.WriteLine($"Id: {tarea.id}, Titulo: {tarea.tittle}");
+        }
+    }
+}
