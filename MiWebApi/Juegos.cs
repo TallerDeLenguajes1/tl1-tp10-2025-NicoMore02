585ac3c1ad426aa289ebe00e3832bdba4614b6bf
Fabricio Nicolas Moreno <nicomore053@gmail.com>
2025-06-17 21:24:16 -0300

    Tercer Ejercicio Resuelto

---
+++ MiWebApi/Juegos.cs
@@ -0,0 +1,64 @@
+using System.Text.Json.Serialization;
+
+namespace JuegosApi
+{
+    class Juegos
+    {
+        [JsonPropertyName("internalName")]
+        public string InternalName { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("metacriticLink")]
+        public string MetacriticLink { get; set; }
+
+        [JsonPropertyName("dealID")]
+        public string DealID { get; set; }
+
+        [JsonPropertyName("storeID")]
+        public string StoreID { get; set; }
+
+        [JsonPropertyName("gameID")]
+        public string GameID { get; set; }
+
+        [JsonPropertyName("salePrice")]
+        public string SalePrice { get; set; }
+
+        [JsonPropertyName("normalPrice")]
+        public string NormalPrice { get; set; }
+
+        [JsonPropertyName("isOnSale")]
+        public string IsOnSale { get; set; }
+
+        [JsonPropertyName("savings")]
+        public string Savings { get; set; }
+
+        [JsonPropertyName("metacriticScore")]
+        public string MetacriticScore { get; set; }
+
+        [JsonPropertyName("steamRatingText")]
+        public string SteamRatingText { get; set; }
+
+        [JsonPropertyName("steamRatingPercent")]
+        public string SteamRatingPercent { get; set; }
+
+        [JsonPropertyName("steamRatingCount")]
+        public string SteamRatingCount { get; set; }
+
+        [JsonPropertyName("steamAppID")]
+        public string SteamAppID { get; set; }
+
+        [JsonPropertyName("realseDate")]
+        public long ReleaseDate { get; set; }
+
+        [JsonPropertyName("lastChange")]
+        public long LastChange { get; set; }
+
+        [JsonPropertyName("dealRating")]
+        public string DealRating { get; set; }
+
+        [JsonPropertyName("thumb")]
+        public string Thumb { get; set; }
+    }
+}
---
+++ MiWebApi/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Api https://www.cheapshark.com/api/1.0/deals?upperPrice=15
+
+using System.Reflection.Metadata;
+using System.Text.Json;
+using JuegosApi;
+
+class program
+{
+    private static readonly HttpClient client = new HttpClient();
+    private const string url = "https://www.cheapshark.com/api/1.0/deals?upperPrice=15";
+
+    static async Task Main(string[] asrg)
+    {
+        var juegos = await GetJuegos();
+        MostrarLista(juegos);
+        crearJson(juegos);
+    }
+
+
+
+    static async Task<List<Juegos>> GetJuegos()
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        List<Juegos> listaJuegos = JsonSerializer.Deserialize<List<Juegos>>(responseBody);
+        return listaJuegos;
+    }
+
+    static void MostrarLista(List<Juegos> juegos)
+    {
+        foreach (var juego in juegos)
+        {
+            Console.WriteLine("Nombre: " + juego.Title);
+            Console.WriteLine($"Precio con descuento: ${juego.SalePrice} || Precio original: ${juego.NormalPrice} te ahorras un: {juego.Savings}%");
+            Console.WriteLine($"Rating: {juego.SteamRatingPercent}%");
+        }
+    }
+
+    static void crearJson(List<Juegos> juegos)
+    {
+        string json = JsonSerializer.Serialize(juegos, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText("juegos.json", json);
+    }
+}
